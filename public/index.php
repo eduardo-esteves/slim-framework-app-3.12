7abf445f0e70c7f3613698676466599327b5c1b1
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-01-25 20:14:40 -0300

    developed first class controller to load on mvc pattern in the calls of route lists

--- public/index.php
+++ public/index.php
@@ -2,14 +2,14 @@
 
 require "../bootstrap.php";
 
+use app\controllers\HomeController;
+
 $app->group('/admin', function($app) {
     $app->get('/conf', function() {
         return phpinfo();
     });
 });
 
-$app->get('/', function() {
-    return 'Hello Slim Framework';
-});
+$app->get('/', HomeController::class . ':index');
 
 $app->run();
