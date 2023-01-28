98c3d0deecd1e3d98f8ebb93b4beaf60a175f1ef
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-01-28 18:53:58 -0300

    created two new function helpers for give support in all structure of the code

--- app/functions/helpers.php
+++ app/functions/helpers.php
@@ -1,1 +1,13 @@
 <?php
+
+function dd($data) {
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+    die();
+}
+
+function path(): string {
+    $vendorDir = dirname(__DIR__);
+    return dirname($vendorDir);
+}
