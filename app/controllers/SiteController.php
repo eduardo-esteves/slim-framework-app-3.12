97705f2c1c3d87627b944a9dfaf53b3a5253bd46
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-01-29 19:28:11 -0300

    instanced Post model to get all posts and show on home view

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -2,11 +2,17 @@
 
 namespace App\controllers;
 
+use App\models\Post;
+
 class SiteController extends Controller {
     public function index() {
+        $post   = new Post();
+        $posts  = $post->all();
+
         $this->view('home', [
             'nome'  => 'Eduardo',
             'title' => 'Home',
+            'posts' => $posts,
         ]);
     }
 
